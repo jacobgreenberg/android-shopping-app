ba900390109650acc983b838c572150c24b298fb
Jacob Greenberg <jmgreenberg@loyola.edu>
2020-11-17 12:31:42 -0500

    create account layout

---
+++ app/src/main/java/dev/prsm/shopping_app/Login.java
@@ -0,0 +1,5 @@
+package dev.prsm.shopping_app;
+
+public class Login
+{
+}
--- app/src/main/java/dev/prsm/shopping_app/MainActivity.java
+++ app/src/main/java/dev/prsm/shopping_app/MainActivity.java
@@ -2,15 +2,34 @@ package dev.prsm.shopping_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createAccount = findViewById(R.id.create_account_button);
+        Button login = findViewById(R.id.login_button);
+
+        createAccount.setOnClickListener((View v) ->
+        {
+            intent = new Intent(this, CreateAccount.class);
+            startActivity(intent);
+        });
+
+        login.setOnClickListener((View v) ->
+        {
+            intent = new Intent(this, Login.class);
+            startActivity(intent);
+        });
     }
 }
