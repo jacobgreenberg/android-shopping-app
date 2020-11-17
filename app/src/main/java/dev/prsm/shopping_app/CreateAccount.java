cec4fc3868e251e43eed96b7cfbc2803eb335714
Jacob Greenberg <jmgreenberg@loyola.edu>
2020-11-17 12:37:30 -0500

    login view

--- app/src/main/java/dev/prsm/shopping_app/CreateAccount.java
+++ app/src/main/java/dev/prsm/shopping_app/CreateAccount.java
@@ -1,5 +1,23 @@
 package dev.prsm.shopping_app;
 
-public class CreateAccount
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount extends AppCompatActivity
 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        Button submit = findViewById(R.id.create_account_submit);
+
+        submit.setOnClickListener((View v) -> {
+            // create the account
+        });
+    }
 }
--- app/src/main/java/dev/prsm/shopping_app/Login.java
+++ app/src/main/java/dev/prsm/shopping_app/Login.java
@@ -1,5 +1,26 @@
 package dev.prsm.shopping_app;
 
-public class Login
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity
 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button submit = findViewById(R.id.login_submit);
+
+        submit.setOnClickListener((View v) ->
+        {
+            // do something here
+        });
+    }
 }
